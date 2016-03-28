a891ed068ca530709a8ea22b25c067c5156ca81d
Viscoze <vlad.oganov@gmail.com>
2016-03-28 11:50:19 +0300

    Add helpers for working with HTML

--- MainFrame.java
+++ MainFrame.java
@@ -1,13 +1,12 @@
 import java.awt.*;
+import java.util.Hashtable;
 import javax.swing.*;
 
 public class MainFrame {
 
     private JFrame frame;
-    private JPanel tablePanel;
-    private String tableHTML;
-    private final int FRAME_SIZE_X = 512;
-    private final int FRAME_SIZE_Y = 384;
+    private final int FRAME_SIZE_X = 1024;
+    private final int FRAME_SIZE_Y = 768;
 
     public MainFrame() {
         this.frame = new JFrame();
@@ -27,10 +26,10 @@ public class MainFrame {
     }
 
     private void initializeUserInterface() {
-        JPanel mainPanel  = new JPanel(new BorderLayout());
+        JPanel mainPanel  = new JPanel(new BorderLayout(3,1));
         JPanel menuPanel  = new JPanel();
         JPanel pagePanel  = new JPanel();
-        this.tablePanel   = new JPanel();
+        JPanel tablePanel   = new JPanel();
 
         JButton createButton = new JButton("Create");
         JButton searchButton = new JButton("Search");
@@ -44,22 +43,40 @@ public class MainFrame {
         menuPanel.add(searchButton);
         menuPanel.add(deleteButton);
 
-        renderTable();
+        tablePanel.add(renderTable(getHeader(3)));
 
-        mainPanel.add(menuPanel, BorderLayout.NORTH);
+        JButton firstButton = new JButton("First Page");
+        JButton lastButton  = new JButton("Last Page");
+        JButton nextButton  = new JButton("Next Page >");
+        JButton prevButton  = new JButton("< Prev Page");
+
+        pagePanel.add(firstButton);
+        pagePanel.add(prevButton);
+        pagePanel.add(nextButton);
+        pagePanel.add(lastButton);
+
+        mainPanel.add(menuPanel, BorderLayout.PAGE_START);
         mainPanel.add(tablePanel, BorderLayout.CENTER);
-        mainPanel.add(pagePanel, BorderLayout.SOUTH);
+        mainPanel.add(pagePanel, BorderLayout.PAGE_END);
 
         frame.add(mainPanel);
     }
 
     private void initializeMenuBar() {
-        JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu   = new JMenu("File");
+        JMenuBar menuBar  = new JMenuBar();
+        JMenu fileMenu    = new JMenu("File");
 
-        JMenuItem open = new JMenuItem("Open");
+        JMenuItem menuOpen = new JMenuItem("Open");
+        JMenuItem menuSave = new JMenuItem("Save");
+        JMenuItem menuExit = new JMenuItem("Exit");
 
-        fileMenu.add(open);
+        menuExit.addActionListener(e -> frame.dispose());
+
+        fileMenu.add(menuOpen);
+        fileMenu.add(menuSave);
+        fileMenu.addSeparator();
+        fileMenu.add(menuExit);
+
         menuBar.add(fileMenu);
 
         frame.setJMenuBar(menuBar);
@@ -74,20 +91,39 @@ public class MainFrame {
         dialog.setResizable(false);
     }
 
-    private void renderTable() {
-        String html="<table style=\"width:100%\" border=\"1\">\n" +
-                "  <tr>\n" +
-                "    <td>Jill</td>\n" +
-                "    <td>Smith</td> \n" +
-                "    <td>50</td>\n" +
-                "  </tr>\n" +
-                "  <tr>\n" +
-                "    <td>Eve</td>\n" +
-                "    <td>Jackson</td> \n" +
-                "    <td>94</td>\n" +
-                "  </tr>\n" +
+    private JEditorPane renderTable(String tableHTML) {
+        JEditorPane table = new JEditorPane("text/html",tableHTML);
+        table.setEditable(false);
+        return table;
+    }
+
+    private String getHeader(int amountOfExams) {
+        return  "<table border='1' style=\"width:100%;text-align:center;\">\n" +
+                "    <tr>\n" +
+                "        <td rowspan=\"3\">Surname</td>\n" +
+                "        <td rowspan=\"3\">Group</td>\n" +
+                "        <td colspan=\"2\">Exams</td>\n" +
+                "    </tr>\n" + getExamsCol(amountOfExams) +
                 "</table>";
-        JEditorPane ed1 = new JEditorPane("text/html",html);
-        tablePanel.add(ed1);
+    }
+
+    private String getExamsCol(int amountOfExams) {
+        String examsHTML = "";
+        for (int i = 0; i < amountOfExams; i++) {
+            examsHTML +="    <tr>" +
+                        "        <td colspan=\"2\">" + (i+1) + "</td>" +
+                        "    </tr>" +
+                        "    <tr>" +
+                        "        <td>Subject</td>" +
+                        "        <td>Mark</td>" +
+                        "    </tr>";
+        }
+        return examsHTML;
+    }
+
+    private String getStudentRow(String surname, String group, Hashtable<String, Integer> subjectToMark) {
+        String studentRow = "<tr><td>" + surname + "</td><td>" + group + "</td>";
+
+        return (studentRow+"</tr>");
     }
 }
